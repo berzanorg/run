a7b08b944f543da33a83eb238fcddb1102cfbc3a
Berzan <berzan@tutanota.com>
2022-10-11 19:43:13 +0300

    update: execute method

--- src/script.rs
+++ src/script.rs
@@ -1,23 +1,23 @@
 use std::process::Command;
 
 /// A script has a command and an info.
-pub struct Script {
+pub struct Script<'a> {
     /// The command of the script.
-    pub command: String,
+    pub command: &'a str,
     /// The info about the script.
-    pub info: String,
+    pub info: &'a str,
 }
 
-impl Script {
+impl<'a> Script<'a> {
     /// Creates a new `Script`.
     /// # Usage
     /// ```rs
     /// let script = Script::new("echo hello");
     /// ```
-    pub fn new(command: String) -> Script {
+    pub fn new(command: &'a str) -> Script<'a> {
         Script {
             command,
-            info: "this script has no info".to_string(),
+            info: "this script has no info",
         }
     }
 
@@ -26,18 +26,20 @@ impl Script {
     /// ```rs
     /// let script_greet = Script::new("echo hello", "Prints a greeting message.");
     /// ```
-    pub fn new_with_info(command: String, info: String) -> Script {
+    pub fn new_with_info(command: &'a str, info: &'a str) -> Script<'a> {
         Script { command, info }
     }
 
     /// Executes `Script`. Returns its exit code.
-    pub fn execute(&self) -> i32 {
+    pub fn execute(&self, path: &str) -> i32 {
         let (shell, c) = if cfg!(target_os = "windows") {
             ("cmd", "/C")
         } else {
             ("sh", "-c")
         };
+
         Command::new(shell)
+            .env("PATH", path)
             .arg(c)
             .arg(&self.command)
             .status()
