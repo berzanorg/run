ceeb7b8abdd250f92cba997fbaf795f27126c538
Berzan <berzan@tutanota.com>
2022-10-12 17:19:30 +0300

    add: DatabaseError

--- src/database/errors.rs
+++ src/database/errors.rs
@@ -1,3 +1,22 @@
+/// The error type for database operations.
+pub enum DatabaseError<'a> {
+    NoAlias(char),
+    MultiAlias(char),
+    NoName(&'a str),
+}
+
+impl<'a> ToString for DatabaseError<'a> {
+    fn to_string(&self) -> String {
+        match self {
+            Self::MultiAlias(alias) => {
+                format!("there are multiple names starting with '{}'", alias)
+            }
+            Self::NoAlias(alias) => format!("there is no name starting with '{}'", alias),
+            Self::NoName(name) => format!("there isn't a script called '{}'", name),
+        }
+    }
+}
+
 /// The error type for script format.
 pub enum FormatError {
     MinusInStartOfName,
@@ -22,7 +41,7 @@ pub enum ParseError {
 }
 
 impl ParseError {
-    pub fn from(err: FormatError, line_no: usize) -> ParseError {
+    pub fn from_parse_error(err: FormatError, line_no: usize) -> ParseError {
         match err {
             FormatError::MinusInStartOfName => ParseError::MinusInStartOfName(line_no),
             FormatError::NoCommand => ParseError::NoCommand(line_no),
@@ -36,19 +55,17 @@ impl ParseError {
 impl ToString for ParseError {
     fn to_string(&self) -> String {
         match self {
-            Self::MinusInStartOfName(line_no) => format!(
-                "LINE {}: minus in start of script names is not allowed",
-                line_no
-            ),
+            Self::MinusInStartOfName(line_no) => {
+                format!("LINE {}: a name cannot start with a minus symbol", line_no)
+            }
             Self::NoColon(line_no) => {
                 format!("LINE {}: seperate name and script with a colon", line_no)
             }
-            Self::NoCommand(line_no) => format!("LINE {}: empty scripts are not allowed", line_no),
-            Self::NoName(line_no) => format!("LINE {}: empty names are not allowed", line_no),
-            Self::SpaceInName(line_no) => format!(
-                "LINE {}: space symbols inside names are not allowed",
-                line_no
-            ),
+            Self::NoCommand(line_no) => format!("LINE {}: script is empty", line_no),
+            Self::NoName(line_no) => format!("LINE {}: name is empty", line_no),
+            Self::SpaceInName(line_no) => {
+                format!("LINE {}: a name cannot contain a space symbol", line_no)
+            }
             Self::UnexpectedComment(line_no) => format!("LINE {}: unexpected comment", line_no),
             Self::UsedName(line_no) => format!("LINE {}: same name is used before", line_no),
         }
