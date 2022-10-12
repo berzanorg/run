ddd513ab47904cf6de4398f1e386bd6a5680b480
Berzan <berzan@tutanota.com>
2022-10-12 18:25:32 +0300

    update: error handling

--- src/database/parse/run_yaml.rs
+++ src/database/parse/run_yaml.rs
@@ -3,6 +3,8 @@ use crate::{
     script::Script,
 };
 
+const RUN_YAML: &'static str = "run.yaml";
+
 impl<'a> Database<'a> {
     pub fn from_run_yaml(run_yaml: &'a str) -> Result<Database<'a>, ParseError> {
         let mut db = Database::new();
@@ -16,24 +18,34 @@ impl<'a> Database<'a> {
                     if last_comment.is_none() {
                         last_comment = Some(&line[1..].trim());
                     } else {
-                        return Err(ParseError::UnexpectedComment(line_index + 1));
+                        return Err(ParseError::UnexpectedComment(line_index + 1, RUN_YAML));
                     }
                 } else {
                     match line.split_once(": ") {
                         Some((key, value)) => {
                             let name = key.trim();
-                            let script = Script::new_with_info(
+                            let script = Script::new_with_comment(
                                 value.trim(),
-                                last_comment.unwrap_or("No information specified."),
+                                last_comment
+                                    .and_then(|comment| {
+                                        Some(if comment.is_empty() {
+                                            "No comment specified."
+                                        } else {
+                                            comment
+                                        })
+                                    })
+                                    .unwrap_or("No comment specified."),
                             );
                             last_comment = None;
 
                             match db.add(name, script) {
                                 Ok(()) => (),
-                                Err(err) => return Err(ParseError::from(err, line_index + 1)),
+                                Err(err) => {
+                                    return Err(err.into_parse_error(line_index + 1, RUN_YAML))
+                                }
                             }
                         }
-                        None => return Err(ParseError::NoColon(line_index + 1)),
+                        None => return Err(ParseError::NoColon(line_index + 1, RUN_YAML)),
                     }
                 }
             }
