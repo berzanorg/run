e3ec4bf771d1b676ffeb4f6bb5653526d04b0518
Berzan <berzan@tutanota.com>
2022-10-12 17:16:30 +0300

    add: new functions

--- src/script.rs
+++ src/script.rs
@@ -1,11 +1,14 @@
+use std::env::{join_paths, split_paths};
+use std::ffi::OsString;
+use std::path::PathBuf;
 use std::process::Command;
 
-/// A script has a command and an info.
+/// A script has a command and a comment.
 pub struct Script<'a> {
     /// The command of the script.
-    pub command: &'a str,
-    /// The info about the script.
-    pub info: &'a str,
+    command: &'a str,
+    /// The comment for the script.
+    comment: &'a str,
 }
 
 impl<'a> Script<'a> {
@@ -17,33 +20,64 @@ impl<'a> Script<'a> {
     pub fn new(command: &'a str) -> Script<'a> {
         Script {
             command,
-            info: "this script has no info",
+            comment: "This script has no comment.",
         }
     }
 
     /// Creates a new `Script`.
     /// # Usage
     /// ```rs
-    /// let script_greet = Script::new("echo hello", "Prints a greeting message.");
+    /// let script_greet = Script::new("echo hey!", "Prints a greeting message.");
     /// ```
-    pub fn new_with_info(command: &'a str, info: &'a str) -> Script<'a> {
-        Script { command, info }
+    pub fn new_with_comment(command: &'a str, comment: &'a str) -> Script<'a> {
+        Script { command, comment }
     }
 
-    /// Executes `Script`. Returns its exit code.
-    pub fn execute(&self, path: &str) -> i32 {
-        let (shell, c) = if cfg!(target_os = "windows") {
-            ("cmd", "/C")
-        } else {
-            ("sh", "-c")
-        };
-
-        Command::new(shell)
-            .env("PATH", path)
-            .arg(c)
-            .arg(&self.command)
-            .status()
-            .and_then(|status| Ok(status.code().unwrap_or(1)))
-            .unwrap_or(1)
+    /// Returns `self.command`.
+    pub fn command(&self) -> &'a str {
+        self.command
     }
+
+    /// Returns `self.comment`.
+    pub fn comment(&self) -> &'a str {
+        self.comment
+    }
+
+    /// Executes `self.command`, then returns the exit code.
+    pub fn execute(&self, extra_path: Option<&'static str>) -> i32 {
+        // Get default shell program and c.
+        let (shell, c) = get_shell_and_c();
+
+        // Get $PATH variable with `extra_path`, if `extra_path` is not `None`.
+        let path_var = extra_path.and_then(|extra_path| get_path_var_with(extra_path));
+
+        // Create a `Command` for launching shell program.
+        let mut command = Command::new(shell);
+
+        // If `extra_path` is specified, set "PATH" environment variable to `path_var`.
+        // Else, continue with the default environment variables.
+        match path_var {
+            Some(path_var) => command.arg(c).arg(&self.command).env("PATH", path_var),
+            None => command.arg(c).arg(&self.command),
+        }
+        .status()
+        .and_then(|status| Ok(status.code().unwrap_or(1)))
+        .unwrap_or(1)
+    }
+}
+
+/// Returns shell and c based on client OS.
+fn get_shell_and_c() -> (&'static str, &'static str) {
+    if cfg!(target_os = "windows") {
+        ("cmd", "/C")
+    } else {
+        ("sh", "-c")
+    }
+}
+
+/// Adds `extra_path` to `$PATH` environment variable, then returns it.
+fn get_path_var_with(extra_path: &'static str) -> Option<OsString> {
+    let mut paths: Vec<PathBuf> = split_paths(&std::env::var_os("PATH")?).collect();
+    paths.push(PathBuf::from(extra_path));
+    join_paths(paths).ok()
 }
