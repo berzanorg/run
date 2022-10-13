2679f7f4184b548ab1f263aa17d427e75c1b1f75
Berzan <berzan@tutanota.com>
2022-10-13 21:05:41 +0300

    update: comments

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use database::{Database, DENO_JSON, PACKAGE_JSON, RUN_YAML};
 
 use crate::exit::Exit;
 
+use beautify::Beautify;
+
+mod beautify;
 mod database;
 mod exit;
 mod file;
@@ -16,25 +19,29 @@ fn main() {
     // Convert it to `Option<&str>`.
     let param = first_arg.as_ref().and_then(|arg| arg.to_str());
 
-    // Match `param`.
+    // Match parameter.
     match param {
         // If initialization flag is set, initialize a new run.yaml file.
         Some("-i" | "--init") => {
             // If package.json file exists in current directory, generate a script database using package.json scripts.
             if file::exists(PACKAGE_JSON) {
                 let package_json = file::read(PACKAGE_JSON).exit();
+
                 let db = Database::from_package_json(&package_json).exit();
+
                 db.save().exit();
             }
             // If deno.json file exists in current directory, generate a script database using deno.json tasks.
             else if file::exists(DENO_JSON) {
                 let deno_json = file::read(DENO_JSON).exit();
+
                 let db = Database::from_deno_json(&deno_json).exit();
                 db.save().exit();
             }
             // If no file above exists in current directory, generate a script database using example.
             else {
                 let db = Database::from_example();
+
                 db.save().exit();
             }
         }
@@ -47,6 +54,7 @@ fn main() {
         // If an alias or name is given, run the script associated with it.
         Some(alias_or_name) => {
             let run_yaml = file::read(RUN_YAML).exit();
+
             let db = Database::from_run_yaml(&run_yaml).exit();
 
             let exit_code = db.run(alias_or_name).exit();
@@ -57,6 +65,7 @@ fn main() {
         // If no arg is given, print all the available scripts.
         None => {
             let run_yaml = file::read(RUN_YAML).exit();
+
             let database = Database::from_run_yaml(&run_yaml).exit();
 
             database.print();
